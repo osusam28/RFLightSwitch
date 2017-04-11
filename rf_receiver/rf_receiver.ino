1e3e2c6a941b851cbf9a38af66dce8c6996cbf25
osusam28 <osusam28@gmail.com>
2017-04-10 23:51:13 -0400

    Add files via upload

---
+++ rf_receiver/rf_receiver.ino
@@ -0,0 +1,48 @@
+#include <VirtualWire.h>
+
+const int led_pin = 13;
+const int receive_pin = 2;
+const int relay_pin = 11;
+
+bool toggle = false;
+
+void setup()
+{
+    // Initialise the IO and ISR
+    vw_set_rx_pin(receive_pin);
+    vw_setup(2000);	 // Bits per sec
+
+    vw_rx_start();       // Start the receiver PLL running
+
+    pinMode(led_pin, OUTPUT);
+    pinMode(relay_pin, OUTPUT);
+}
+
+void loop()
+{
+    uint8_t buf[VW_MAX_MESSAGE_LEN];
+    uint8_t buflen = VW_MAX_MESSAGE_LEN;
+    char c;
+
+    if (vw_get_message(buf, &buflen)) // Non-blocking
+    {
+        for (int i = 0; i < buflen; i++)
+	{
+	    c = (char)buf[i];
+	}
+        if(c == 's')
+        {
+          digitalWrite(led_pin, HIGH);
+          toggle = !toggle;
+          if(toggle == true)
+          {
+            digitalWrite(relay_pin, HIGH);
+          }
+          else
+          {
+            digitalWrite(relay_pin, LOW);
+          }
+          digitalWrite(led_pin, LOW);
+        }
+    }
+}
---
+++ sleep_transmitter/sleep_transmitter.ino
@@ -0,0 +1,60 @@
+#include <LowPower.h>
+#include <avr/sleep.h>
+#include <VirtualWire.h>
+
+int led_pin = 13;
+const int transmit_pin = 3;
+const int button_pin = 2;
+const int button_led = 10;
+
+void setup()
+{
+  vw_set_tx_pin(transmit_pin);
+  vw_setup(2000);
+  pinMode(led_pin, OUTPUT);
+  pinMode(2, INPUT_PULLUP);
+  pinMode(button_led, OUTPUT);
+  digitalWrite(button_led, HIGH);
+  interrupts();
+}
+
+byte count = 1;
+bool button_pressed = false;
+
+void loop()
+{
+//  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+//  sleep_enable();
+  
+  attachInterrupt(0, awake, LOW);
+  
+  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+//  sleep_mode();
+  
+  detachInterrupt(0);
+  
+  char msg[1] = {'s'};
+  
+  if(digitalRead(button_pin) == HIGH && button_pressed == false)
+  {
+    digitalWrite(button_led, LOW); 
+    digitalWrite(led_pin, HIGH); // Flash a light to show transmitting
+    vw_send((uint8_t *)msg, 1);
+    vw_wait_tx(); // Wait until the whole message is gone
+    delay(100);
+    digitalWrite(button_led, HIGH);
+    digitalWrite(led_pin, LOW);
+    button_pressed = true;
+    delay(500);
+  }
+  
+  if(digitalRead(button_pin) == LOW)
+  {
+    button_pressed = false;
+  }
+}
+
+void awake()
+{
+  
+}
